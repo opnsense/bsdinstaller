fe2bb6bd3289b6f0be97802081e2414f2cecd934
Franco Fichtner <franco@opnsense.org>
2018-12-23 15:35:53 +0100

    installer: tweak wording as live settings are retained
    (Also when previously imported.)

--- installer/easy_install/200_select_disk.lua
+++ installer/easy_install/200_select_disk.lua
@@ -46,10 +46,11 @@ return {
 	local dd = StorageUI.select_disk{
 	    sd = App.state.storage,
 	    short_desc = _(
-		"This will automatically install %s without asking many questions.\n\n" ..
+		"This will automatically install %s with all of its current " ..
+		"settings without asking many questions.\n\n" ..
 		"WARNING: All contents of the selected hard disk will be erased! " ..
 		"This action is irreversible. Do you really want to continue?\n\n" ..
-		"Select a disk to continue.",
+		"Select a disk to continue:",
 	        App.conf.product.name),
 	    cancel_desc = _("Return to %s", step:get_prev_name())
 	}
