dac8ac1b1b3cc76a7946483340fb0586686673f7
Franco Fichtner <franco@opnsense.org>
2018-12-28 12:32:35 +0100

    installer: also gone

--- installer/conf/BSDInstaller.lua
+++ installer/conf/BSDInstaller.lua
@@ -376,7 +376,6 @@ cmd_names = {
 	-- These aren't commands, but they're configurable here nonetheless.
 
 	DMESG_BOOT	= "var/run/dmesg.boot",
-	MODULES_DIR	= "boot/kernel",
 	SYSCTL_DISKS	= "kern.disks"
 }
 
